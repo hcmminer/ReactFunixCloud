8dc2751fd8f3c3af8c66a677e1f6d04b87a032fc
ban <bandvfx17188@funix.edu.vn>
2022-07-31 22:06:11 +0700

    ntrolled Forms

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Main from "./components/MainComponent";
 import { BrowserRouter } from "react-router-dom";
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <Main />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
